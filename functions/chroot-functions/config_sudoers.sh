aeb4ceecca6c5ac90e6b60e315caf210ceb5993a
Bharat Middha <bmiddha99@gmail.com>
2019-01-31 02:53:27 -0600

    added sudo insults

--- functions/chroot-functions/config_sudoers.sh
+++ functions/chroot-functions/config_sudoers.sh
@@ -1,7 +1,8 @@
 # Function to give sudo access to users, and groups
 function config_sudoers {
 
-cat << EOM > /etc/sudoers.d/AcmAdmins
+cat << EOM > /etc/sudoers.d/acm
+Defaults 	insults
 $ADMIN_USERNAME ALL=(ALL) ALL
 %AcmLanAdmins ALL=(ALL) ALL
 EOM
